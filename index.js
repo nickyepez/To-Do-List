7d4256e8f30575b05dd65e2eb4a4a21730e19711
Nick Yepez <nickyepez@NICKs-Air.attlocal.net>
2023-01-23 21:50:47 -0600

    Refactor by creating a Form.js file and simplifying the code - easier to read.

--- index.js
+++ index.js
@@ -17,36 +17,24 @@ function App() {
             isCompleted: false,
         }
     ]);
-    const [value, setValue] = React.useState('');
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (!value) return;
-        const newTodos = [...todos, { text: value, isCompleted: false }];
+
+    const addTodo = text => {
+        const newTodos = [...todos, {text:text, isCompleted:false}];
         setTodos(newTodos);
-        setValue('');
     }
+
     const removeTodo = e => {
         const index = Number(e.target.id);
         let temp = [...todos];
         temp.splice(index, 1);
         setTodos(temp);
     }
-    return (
-        <>
-            {todos.map((todo, i) =>
-                <div className="todo" key={i} id={i} onClick={removeTodo}>
-                    {todo.text}</div>)}
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    className="input"
-                    value={value}
-                    placeholder="Add Todo ..."
-                    onChange={e => setValue(e.target.value)}
-                />
-            </form>
-        </>
-    );
+    return (<>
+        {todos.map((todo,i) =>
+            <div className="todo" key={i} id={i} onClick={removeTodo}>
+            {todo.text}</div>)}
+        <TodoForm addTodo={addTodo}/>
+    </>);
 }
 
 ReactDOM.render(
